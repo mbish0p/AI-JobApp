89ec5024a7ced96b64815839609328978812eb7c
mateusz <biskup.mateusz1997@wp.pl>
2020-09-17 12:10:13 +0200

    Messages divide by sides

--- client/src/Chatbot/Chatbot_v2.js
+++ client/src/Chatbot/Chatbot_v2.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import axios from 'axios'
 import { useDispatch, useSelector } from 'react-redux'
 import { saveMessage, deleteMessage } from '../_actions/message_actions'
--- client/src/Chatbot/Sections/Message.js
+++ client/src/Chatbot/Sections/Message.js
@@ -4,10 +4,11 @@ import { List, Icon, Avatar } from 'antd';
 function Message(props) {
 
     const AvatarSrc = props.who === 'Arnold' ? <Icon type="robot" /> : <Icon type="smile" />
-
+    const who = props.who === 'Arnold' ? 'bot-message' : 'user-message'
     return (
         <List.Item style={{ padding: '1rem' }}>
             <List.Item.Meta
+                className={who + ' message-container'}
                 avatar={<Avatar icon={AvatarSrc} />}
                 title={props.who}
                 description={props.text}
