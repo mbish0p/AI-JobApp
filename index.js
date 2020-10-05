11e3ff89c75910a7114a75ea91ecd60cbe616d73
mateusz <biskup.mateusz1997@wp.pl>
2020-10-06 00:18:08 +0200

    Succesful add sequalize and connect with db

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
+const { Sequelize } = require('sequelize')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/api/dialogflow', require('./server/routes/dialogflow'));
 
-app.get('/', (req, res) => {
-    res.send('adasdasdasd')
+const sequelize = new Sequelize('AI-JobApp', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+app.get('/', async (req, res) => {
+
+    try {
+        await sequelize.authenticate();
+        res.send('Connection has been established successfully.');
+    } catch (error) {
+        res.send('Unable to connect to the database:', error);
+    }
+
+
 })
 
 
