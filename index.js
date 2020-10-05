79d37f70be2ed5a4b4071e6f7171fcfbe341c232
mateusz <biskup.mateusz1997@wp.pl>
2020-10-06 01:45:39 +0200

    Create user model

--- index.js
+++ index.js
@@ -2,28 +2,33 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
-const { Sequelize } = require('sequelize')
+const sequelize = require('./server/db/sequelize')
+
+const User = require('./server/models/User')
+User.sync().then(() => {
+    console.log('Succesful synchronized user table')
+}).catch((error) => {
+    console.log('Something went wrong with synchronized user table', error)
+})
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/api/dialogflow', require('./server/routes/dialogflow'));
 
-const sequelize = new Sequelize('AI-JobApp', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
 app.get('/', async (req, res) => {
-
     try {
-        await sequelize.authenticate();
-        res.send('Connection has been established successfully.');
+        const user = await User.create({
+            name: "Dawid",
+            surname: "Duda",
+            password: 'haslo',
+            email: 'dupa@dupa.dupa',
+        })
+        await user.save()
+        res.send(user);
     } catch (error) {
         res.send('Unable to connect to the database:', error);
     }
-
-
 })
 
 
