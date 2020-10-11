7679e49722b0edda72746043234081d63fe917ac
mateusz <biskup.mateusz1997@wp.pl>
2020-10-11 18:05:29 +0200

    Create connection with azure blob storage

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const sequalize = require('./server/db/sequelize');
+//const sequalize = require('./server/db/sequelize');
 
 // (async () => {
 //     try {
@@ -11,6 +11,7 @@ const sequalize = require('./server/db/sequelize');
 //     }
 // })()
 
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,6 +21,26 @@ app.use('/users', require('./server/routes/user'))
 
 
 const port = process.env.PORT || 5000;
+
+const keys = require('./server/config/dev')
+const { BlobServiceClient } = require("@azure/storage-blob");
+
+const STORAGE_ACCOUNT_NAME = keys.AZURE_STORAGE_ACCOUNT_NAME
+const ACCOUNT_ACCESS_KEY = keys.AZURE_STORAGE_ACCOUNT_ACCESS_KEY
+const SAS = keys.AZURE_SHARED_ACCESS_SIGNATURE
+
+const blobServiceClient = new BlobServiceClient(`https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net${SAS}`);
+
+async function main() {
+    let i = 1;
+    let containers = blobServiceClient.listContainers();
+    for await (const container of containers) {
+        console.log(`Container ${i++}: ${container.name}`);
+    }
+}
+
+main()
+
 
 app.listen(port, () => {
     console.log(`Server Running at ${port}`)
