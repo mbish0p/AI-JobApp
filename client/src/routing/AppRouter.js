d824cead1c7045f44d811c06a9caef45e4cf49b0
mateusz <biskup.mateusz1997@wp.pl>
2020-10-23 17:41:36 +0200

    Create login from and handle login errors

--- client/src/routing/AppRouter.js
+++ client/src/routing/AppRouter.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Login from '../Components/Login'
 import About from '../Components/About'
-import Dashboard from '../Components/Dashboard'
 import User from '../Components/User'
 import App from '../Components/App'
 
@@ -10,10 +9,10 @@ const AppRouter = () => {
     return (
         <BrowserRouter>
             <Switch>
+                <Route exact={true} path="/" component={Login} />
                 <Route exact={true} path="/about" component={About} />
-                <Route exact={true} path="/login" component={Login} />
                 <Route exact={true} path="/user" component={User} />
-                <Route exact={true} path="/" component={App} />
+                <Route exact={true} path="/dashboard" component={App} />
             </Switch>
         </BrowserRouter>
     )
